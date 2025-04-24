f7e84a66174ff8314b409bd186427b3dd83ee55e
Evgeniy <charizardik64@gmail.com>
2025-04-24 14:36:27 +0300

    button styles changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { PokemonListScreen } from './screens/PokemonListScreen.tsx'
-import { PokemonListItemDetailsScreen } from './screens/PokemonListItemDetailsScreen.tsx'
-import { ComparisonPokemonScreen } from './screens/ComparisonPokemonScreen.tsx'
 import { FavoritePokemonScreen } from './screens/FavoritePokemonScreen.tsx'
+import { ComparisonPokemonScreen } from './screens/ComparisonPokemonScreen.tsx'
+// import { PokemonListScreen } from './screens/PokemonListScreen.tsx'
+// import { PokemonListItemDetailsScreen } from './screens/PokemonListItemDetailsScreen.tsx'
 
 export const App = () => {
   return (<>
       <FavoritePokemonScreen/>
       <ComparisonPokemonScreen/>
 
-      <PokemonListScreen/>
-      <PokemonListItemDetailsScreen />
+      {/*<PokemonListScreen/>*/}
+      {/*<PokemonListItemDetailsScreen />*/}
   </>
   )
 }
