0565723414317f5f9c982e1cca5e88c0abd15ac3
Evgeniy <charizardik64@gmail.com>
2025-05-24 15:23:34 +0300

    test: coverage

--- src/__tests__/getPeriodicNumberFromUrl.test.ts
+++ src/__tests__/getPeriodicNumberFromUrl.test.ts
@@ -2,6 +2,12 @@ import { describe, expect, test } from 'vitest'
 import getPeriodicNumberFromUrl from '../utils/getPeriodicNumberFromUrl.ts'
 
 describe('happy', () => {
+  test('pass "https://pokeapi.co/api/v2/pokemon/000000000000000000/" to equal "000000000000000000"',
+    () => {
+      expect(getPeriodicNumberFromUrl('https://pokeapi.co/api/v2/pokemon/000000000000000000/'))
+        .toEqual('000000000000000000')
+    })
+
   test('pass "https://pokeapi.co/api/v2/pokemon/667/" to equal "667"', () => {
     expect(getPeriodicNumberFromUrl('https://pokeapi.co/api/v2/pokemon/667/')).toEqual('667')
   })
@@ -16,43 +22,47 @@ describe('happy', () => {
 })
 
 describe('edge', () => {
-  test('pass "https://pokemonapi.co/api/v2/pokemon/1/" to equal "1"', () => {
-    expect(getPeriodicNumberFromUrl('https://pokemonapi.co/api/v2/pokemon/1/')).toEqual('1')
+  describe('wrong URL', () => {
+    test('pass "https://pokemonapi.co/api/v2/pokemon/1/" to equal "1"', () => {
+      expect(getPeriodicNumberFromUrl('https://pokemonapi.co/api/v2/pokemon/1/')).toEqual('1')
+    })
+
+    test('pass "https://pokeapi.co/api/v2/pokemon/42" to equal "42"', () => {
+      expect(getPeriodicNumberFromUrl('https://pokeapi.co/api/v2/pokemon/42')).toEqual('42')
+    })
+
+    test('pass "pokeapi.co/api/v2/pokemon/42" to equal "42"', () => {
+      expect(getPeriodicNumberFromUrl('pokeapi.co/api/v2/pokemon/42')).toEqual('42')
+    })
+
+    test('pass "www.pokeapi.co/api/v2/pokemon/42" to equal "42"', () => {
+      expect(getPeriodicNumberFromUrl('www.pokeapi.co/api/v2/pokemon/42')).toEqual('42')
+    })
+
+    test('pass "www.pokeapi.co/api/v3/pokemon/42" to equal "42"', () => {
+      expect(getPeriodicNumberFromUrl('www.pokeapi.co/api/v3/pokemon/42')).toEqual('42')
+    })
+
+    test('pass "https://pokeapi.co/api/v2/pokemon/42///" to equal "42"', () => {
+      expect(getPeriodicNumberFromUrl('https://pokeapi.co/api/v2/pokemon/42///')).toEqual('42')
+    })
+
+    test('pass "https://pokeapi.co/api/v2/pokemon/pikachu/" to equal "pikachu"', () => {
+      expect(getPeriodicNumberFromUrl('https://pokeapi.co/api/v2/pokemon/pikachu')).toEqual('pikachu')
+    })
   })
 
-  test('pass "https://pokeapi.co/api/v2/pokemon/42" to equal "42"', () => {
-    expect(getPeriodicNumberFromUrl('https://pokeapi.co/api/v2/pokemon/42')).toEqual('42')
-  })
+  describe('Not URL', () => {
+    test('pass "hello" to equal "hello"', () => {
+      expect(getPeriodicNumberFromUrl('hello')).toEqual('hello')
+    })
 
-  test('pass "pokeapi.co/api/v2/pokemon/42" to equal "42"', () => {
-    expect(getPeriodicNumberFromUrl('pokeapi.co/api/v2/pokemon/42')).toEqual('42')
-  })
+    test('pass "" to equal ""', () => {
+      expect(getPeriodicNumberFromUrl('')).toEqual('')
+    })
 
-  test('pass "www.pokeapi.co/api/v2/pokemon/42" to equal "42"', () => {
-    expect(getPeriodicNumberFromUrl('www.pokeapi.co/api/v2/pokemon/42')).toEqual('42')
-  })
-
-  test('pass "www.pokeapi.co/api/v3/pokemon/42" to equal "42"', () => {
-    expect(getPeriodicNumberFromUrl('www.pokeapi.co/api/v3/pokemon/42')).toEqual('42')
-  })
-
-  test('pass "https://pokeapi.co/api/v2/pokemon/42///" to equal "42"', () => {
-    expect(getPeriodicNumberFromUrl('https://pokeapi.co/api/v2/pokemon/42///')).toEqual('42')
-  })
-
-  test('pass "https://pokeapi.co/api/v2/pokemon/pikachu/" to equal "pikachu"', () => {
-    expect(getPeriodicNumberFromUrl('https://pokeapi.co/api/v2/pokemon/pikachu')).toEqual('pikachu')
-  })
-
-  test('pass "hello" to equal "hello"', () => {
-    expect(getPeriodicNumberFromUrl('hello')).toEqual('hello')
-  })
-
-  test('pass "" to equal ""', () => {
-    expect(getPeriodicNumberFromUrl('')).toEqual('')
-  })
-
-  test('pass "1" to equal "1"', () => {
-    expect(getPeriodicNumberFromUrl('1')).toEqual('1')
+    test('pass "1" to equal "1"', () => {
+      expect(getPeriodicNumberFromUrl('1')).toEqual('1')
+    })
   })
 })
